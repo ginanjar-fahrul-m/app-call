189a794f73380d7aa4a4a9ed3d548d182d54c4cd
Ginanjar Fahrul Muttaqin <ginanjar@icehousecorp.com>
2015-06-22 16:52:06 +0700

    call another app via intent

--- firstapp/app/src/main/java/com/digitalines/firstapp/MainActivity.java
+++ firstapp/app/src/main/java/com/digitalines/firstapp/MainActivity.java
@@ -1,19 +1,65 @@
 package com.digitalines.firstapp;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Collections;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+
+    private static String FIRST_PARAM = "firstparam";
+
+    private static String SECOND_PARAM = "secondparam";
+
+    private static String SECOND_APP_ID = "com.digitalines.secondapp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((Button) findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callApp();
+            }
+        });
     }
 
+    private void callApp() {
+        String firstParam = ((EditText) findViewById(R.id.editText)).getText().toString();
+        String secondParam = ((EditText) findViewById(R.id.editText2)).getText().toString();
+
+        Bundle params = new Bundle();
+        params.putString(FIRST_PARAM, firstParam);
+        params.putString(SECOND_PARAM, secondParam);
+
+        Intent launchIntent = getPackageManager()
+                .getLaunchIntentForPackage(SECOND_APP_ID);
+
+        if (launchIntent != null) {
+            launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(launchIntent, params);
+        } else {
+            launchIntent = new Intent(Intent.ACTION_VIEW);
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            launchIntent.setData(Uri.parse("market://details?id=" + SECOND_APP_ID));
+            startActivity(launchIntent);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- secondapp/app/src/main/java/com/digitalines/secondapp/MainActivity.java
+++ secondapp/app/src/main/java/com/digitalines/secondapp/MainActivity.java
@@ -1,19 +1,73 @@
 package com.digitalines.secondapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private static String FIRST_PARAM = "firstparam";
+
+    private static String SECOND_PARAM = "secondparam";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Uri data = getIntent().getData();
+        if (data != null) {
+            showParams(data);
+        }
+
+        Intent activityIntent = getIntent();
+        if (activityIntent != null) {
+            Bundle params = activityIntent.getExtras();
+            if (params != null) {
+                showParams(params);
+            }
+        }
     }
 
+    private void showParams(Uri data) {
+        String scheme = data.getScheme();
+        String host = data.getHost();
+
+        if (!scheme.isEmpty() && !host.isEmpty()) {
+            String firstParam = data.getQueryParameter(FIRST_PARAM);
+            String secondParam = data.getQueryParameter(SECOND_PARAM);
+
+            String output = "Data from " + scheme + ":" + host + "/n";
+            if (!firstParam.isEmpty()) {
+                output += "First Param = " + firstParam + "/n";
+            }
+            if (!secondParam.isEmpty()) {
+                output += "Second Param = " + secondParam + "/n";
+            }
+
+            ((TextView) findViewById(R.id.textView)).setText(output);
+        }
+    }
+
+    private void showParams(Bundle params) {
+        String firstParam = params.getString(FIRST_PARAM);
+        String secondParam = params.getString(SECOND_PARAM);
+
+        String output = "Data from another app" + "/n";
+        if (!firstParam.isEmpty()) {
+            output += "First Param = " + firstParam + "/n";
+        }
+        if (!secondParam.isEmpty()) {
+            output += "Second Param = " + secondParam + "/n";
+        }
+
+        ((TextView) findViewById(R.id.textView)).setText(output);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
